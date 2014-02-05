bcb7a0cb836ff3a22d5f63b7db8b68a8198bba52
Frank Migliorino <fam1995@gmail.com>
2014-02-05 15:14:41 -0500

    Initial Commit

---
+++ Base2Conversion/Base2Conversion.vb
@@ -0,0 +1,146 @@
+﻿Public Class Base2Conversion
+
+    'Frank Migliorino - 1/24/2010
+    'Values obtained from http://www.onlineconversion.com/computer_base2.htm
+
+    'Notes
+    '1/8388608      1/1048576        1/1024          1/1
+    'Bit (2^(−23))  Byte (2^(−20))	 KB (2^(−10))	MB (2^0)
+    '1024/1         1048576/1        1073741824/1	
+    'GB (2^10)	    Terabyte (2^20)  Petabyte (2^30)	
+
+    'So,
+
+    '1 MB ->  1024 KB :
+
+    'x * (2^(z−y))
+    'x = input value
+    'y = output exponent
+    'z = input exponent
+
+
+    ''' <summary>
+    ''' The exponental values of prefixes. Ex: BitExp = -23 since 1 Bit = 2^-23 MB.
+    ''' </summary>
+    ''' <remarks></remarks>
+    Enum PrefixExpValues
+        BitExp = -23
+        ByteExp = -20
+        KilobyteExp = -10
+        MegabyteExp = 0
+        GigabyteExp = 10
+        TerabyteExp = 20
+        PetabyteExp = 30
+    End Enum
+
+    Dim cbxStrings As String() = New String() _
+    {"Bits", "Bytes", "Kilobytes (KB)", "Megabytes (MB)", "Gigabytes (GB)", "Terabytes", "Petabytes"}
+
+    ''' <summary>
+    ''' Returns a Long converted from one 2-based prefix to another.
+    ''' </summary>
+    ''' <param name="value">Original Long to be converted</param>
+    ''' <param name="inputType">The original type of number (ex. byte, Kilobyte, etc..)</param>
+    ''' <param name="outputType">The type to convert the number to.</param>
+    ''' <param name="err">Returns any error messages, blank if none.</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Function Convert(ByVal value As Long, ByVal inputType As PrefixExpValues, ByVal outputType As PrefixExpValues, ByRef err As String) As Long
+        Try
+            Return value * (2 ^ (inputType - outputType))
+        Catch ex As OverflowException
+            err = ex.Message
+            Return -1
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Returns a Double converted from one 2-based prefix to another.
+    ''' </summary>
+    ''' <param name="value">Original Double to be converted</param>
+    ''' <param name="inputType">The original type of number (ex. byte, Kilobyte, etc..</param>
+    ''' <param name="outputType">The type to convert the number to.</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Function Convert(ByVal value As Double, ByVal inputType As PrefixExpValues, ByVal outputType As PrefixExpValues) As Double
+        Return value * (2 ^ (inputType - outputType))
+    End Function
+
+    ''' <summary>
+    ''' Returns a Decimal converted from one 2-based prefix to another.
+    ''' </summary>
+    ''' <param name="value">Original Decimal to be converted</param>
+    ''' <param name="inputType">The original type of number (ex. byte, Kilobyte, etc..</param>
+    ''' <param name="outputType">The type to convert the number to.</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Function Convert(ByVal value As Decimal, ByVal inputType As PrefixExpValues, ByVal outputType As PrefixExpValues, ByRef err As String) As Decimal
+        Try
+            Return value * (2 ^ (inputType - outputType))
+        Catch ex As OverflowException
+            Err = ex.Message
+            Return -1
+        End Try
+    End Function
+
+    Sub TypeFromString(ByRef inputType As String, ByRef outputType As String)
+        If inputType = cbxStrings(0) Then
+            inputType = PrefixExpValues.BitExp
+        ElseIf inputType = cbxStrings(1) Then
+            inputType = PrefixExpValues.ByteExp
+        ElseIf inputType = cbxStrings(2) Then
+            inputType = PrefixExpValues.KilobyteExp
+        ElseIf inputType = cbxStrings(3) Then
+            inputType = PrefixExpValues.MegabyteExp
+        ElseIf inputType = cbxStrings(4) Then
+            inputType = PrefixExpValues.GigabyteExp
+        ElseIf inputType = cbxStrings(5) Then
+            inputType = PrefixExpValues.TerabyteExp
+        ElseIf inputType = cbxStrings(6) Then
+            inputType = PrefixExpValues.PetabyteExp
+        End If
+
+        If outputType = cbxStrings(0) Then
+            outputType = PrefixExpValues.BitExp
+        ElseIf outputType = cbxStrings(1) Then
+            outputType = PrefixExpValues.ByteExp
+        ElseIf outputType = cbxStrings(2) Then
+            outputType = PrefixExpValues.KilobyteExp
+        ElseIf outputType = cbxStrings(3) Then
+            outputType = PrefixExpValues.MegabyteExp
+        ElseIf outputType = cbxStrings(4) Then
+            outputType = PrefixExpValues.GigabyteExp
+        ElseIf outputType = cbxStrings(5) Then
+            outputType = PrefixExpValues.TerabyteExp
+        ElseIf outputType = cbxStrings(6) Then
+            outputType = PrefixExpValues.PetabyteExp
+        End If
+    End Sub
+
+
+
+    Function Convert(ByVal value As Long, ByVal inputType As String, ByVal outputType As String, ByRef err As String) As Long
+        TypeFromString(inputType, outputType)
+        Try
+            Return value * (2 ^ (inputType - outputType))
+        Catch ex As OverflowException
+            err = ex.Message
+            Return -1
+        End Try
+    End Function
+
+    Function Convert(ByVal value As Double, ByVal inputType As String, ByVal outputType As String) As Double
+        TypeFromString(inputType, outputType)
+        Return value * (2 ^ (inputType - outputType))
+    End Function
+
+    Function Convert(ByVal value As Decimal, ByVal inputType As String, ByVal outputType As String, ByRef err As String) As Decimal
+        TypeFromString(inputType, outputType)
+        Try
+            Return value * (2 ^ (inputType - outputType))
+        Catch ex As OverflowException
+            err = ex.Message
+            Return -1
+        End Try
+    End Function
+End Class
---
+++ Base2ConversionForm/MainForm.xaml.vb
@@ -0,0 +1,128 @@
+﻿Imports Base2Conversion
+Class MainForm
+
+    Dim cbxStrings As String() = New String() _
+    {"Bits", "Bytes", "Kilobytes (KB)", "Megabytes (MB)", "Gigabytes (GB)", "Terabytes", "Petabytes"}
+
+    Private Base2Conversion As New Base2Conversion.Base2Conversion
+
+    Sub ProcessNumbers(ByVal inputType As String, ByVal outputType As String, ByRef txtIn As System.Windows.Controls.TextBox, ByRef txtOut As System.Windows.Controls.TextBox)
+        Dim temp As String = ""
+        If rdbLong.IsChecked Then temp = "Long"
+        If rdbDecimal.IsChecked Then temp = "Decimal"
+        If rdbDouble.IsChecked Then temp = "Double"
+
+        Select Case temp
+            Case "Long"
+                Dim endVal As Long
+                endVal = Base2Conversion.Convert(CType(txtIn.Text, Long), inputType, outputType)
+
+                txtOut.Text = endVal
+
+            Case "Decimal"
+                Dim endVal As Decimal
+                endVal = Base2Conversion.Convert(CType(txtIn.Text, Decimal), inputType, outputType)
+
+                txtOut.Text = If(Not sldDecimalPlaces.Value = 11, Decimal.Round(endVal, decimals:=sldDecimalPlaces.Value), endVal)
+                lblDecimalPlacesOut.Content = If(Not sldDecimalPlaces.Value = 11, sldDecimalPlaces.Value, "Max")
+            Case "Double"
+                Dim endVal As Double
+                endVal = Base2Conversion.Convert(CType(txtIn.Text, Double), inputType, outputType)
+
+                txtOut.Text = If(Not sldDecimalPlaces.Value = 11, Math.Round(d:=endVal, decimals:=sldDecimalPlaces.Value), endVal)
+                lblDecimalPlacesOut.Content = If(Not sldDecimalPlaces.Value = 11, sldDecimalPlaces.Value, "Max")
+        End Select
+
+
+    End Sub
+
+    Sub ConvertVals(ByVal inputType As String, ByVal outputType As String, ByRef txtIn As System.Windows.Controls.TextBox, ByRef txtOut As System.Windows.Controls.TextBox)
+        If cbxInputType.Text <> "" And cbxOutputType.Text <> "" And txtIn.Text <> "" And IsNumeric(txtIn.Text) Then
+            ProcessNumbers(inputType, outputType, txtIn, txtOut)
+        End If
+    End Sub
+    Private Sub btnConvert_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles btnConvert.Click
+        Try
+            ConvertVals(cbxInputType.Text, cbxOutputType.Text, txtIn, txtOut)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'reversed
+    'UPDATE: not anymore
+    Private Sub cbxOutputType_SelectionChanged(ByVal sender As System.Object, ByVal e As System.Windows.Controls.SelectionChangedEventArgs) Handles cbxOutputType.SelectionChanged
+        Try
+            ConvertVals(cbxInputType.Text, e.AddedItems.Item(0).content, txtIn, txtOut)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub cbxInputType_SelectionChanged(ByVal sender As Object, ByVal e As System.Windows.Controls.SelectionChangedEventArgs) Handles cbxInputType.SelectionChanged
+        Try
+            ConvertVals(e.AddedItems.Item(0).content, cbxOutputType.Text, txtIn, txtOut)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub rdbDouble_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles rdbDouble.Click, rdbDecimal.Click, rdbLong.Click
+        Try
+            ConvertVals(cbxInputType.Text, cbxOutputType.Text, txtIn, txtOut)
+        Catch ex As Exception
+
+        End Try
+        If CType(sender, RadioButton).Name.Contains("Long") Then
+            sldDecimalPlaces.IsEnabled = False
+            lblDecimalPlacesOut.Content = 0
+        Else
+            sldDecimalPlaces.IsEnabled = True
+            lblDecimalPlacesOut.Content = If(Not sldDecimalPlaces.Value = 11, sldDecimalPlaces.Value, "Max")
+        End If
+    End Sub
+    'reversed
+    'UPDATE: Not anymore
+    Private Sub txtOut_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Input.KeyEventArgs) Handles txtOut.KeyUp
+        Try
+            If txtOut.Text - Math.Truncate(Double.Parse(txtOut.Text)) > 0 Then
+                rdbLong.IsEnabled = False
+                If rdbLong.IsChecked Then rdbDecimal.IsChecked = True
+            Else
+                rdbLong.IsEnabled = True
+            End If
+            ConvertVals(cbxInputType.Text, cbxOutputType.Text, txtIn, txtOut)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub txtIn_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Input.KeyEventArgs) Handles txtIn.KeyUp
+        Try
+            If txtIn.Text - Math.Truncate(Double.Parse(txtIn.Text)) > 0 Then
+                rdbLong.IsEnabled = False
+                If rdbLong.IsChecked Then rdbDecimal.IsChecked = True
+            Else
+                rdbLong.IsEnabled = True
+            End If
+            ConvertVals(cbxInputType.Text, cbxOutputType.Text, txtIn, txtOut)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnSwap_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles btnSwap.Click
+        Dim tempStrOut = txtOut.Text
+        Dim tempStrTypeIn = cbxInputType.Text
+        Dim tempStrTypeOut = cbxOutputType.Text 'temp vars used here to avoid any auto-activating methods
+        cbxInputType.Text = tempStrTypeOut
+        cbxOutputType.Text = tempStrTypeIn
+        txtIn.Text = tempStrOut
+        ConvertVals(cbxInputType.Text, cbxOutputType.Text, txtIn, txtOut)
+    End Sub
+
+    Private Sub sldDecimalPlaces_ValueChanged(ByVal sender As System.Object, ByVal e As System.Windows.RoutedPropertyChangedEventArgs(Of System.Double)) Handles sldDecimalPlaces.ValueChanged
+        ConvertVals(cbxInputType.Text, cbxOutputType.Text, txtIn, txtOut)
+        If Not rdbLong.IsChecked Then lblDecimalPlacesOut.Content = If(Not sldDecimalPlaces.Value = 11, sldDecimalPlaces.Value, "Max")
+    End Sub
+
+    Private Sub MainForm_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs) Handles Me.Loaded
+        sldDecimalPlaces.Value = 11
+    End Sub
+End Class
